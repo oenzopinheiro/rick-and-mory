82ad15808fcb09d75e019b57f497a6f8588d3aa6
Enzo Pinheiro <oenzopinheiro@gmail.com>
2023-08-22 18:48:49 -0300

    first commit

--- main.js
+++ main.js
@@ -1,4 +1,25 @@
-const seilaElement = document.querySelector(".seila");
+const rickElement = document.querySelector(".rickAndMoryApi");
+
+let characterString = ""
+
+async function getSingleCharacter(id) {
+  fetch(`https://rickandmortyapi.com/api/character/${id}`)
+  .then((response) => {
+    return response.json();
+  })
+  .then((response) => {
+
+
+    rickElement.innerHTML = `
+    <div class="box">
+    <img src="${response.image}" class="img">
+<div class="list">
+    <div class="getSingleCharacter"> ${response.name}</div>
+    </div>
+    </div>
+    `
+  });
+}
 
 async function init() {
   fetch("https://rickandmortyapi.com/api/character")
@@ -6,9 +27,30 @@ async function init() {
       return response.json();
     })
     .then((response) => {
-      const { info, result } = response;
-      seilaElement.innerHTML = info.next;
-    });
+      const { info, results} = response;
+
+      results.forEach(showCharacters)
+
+      rickElement.innerHTML = `
+      <div class="characters_container">${characterString}
+      </div>`
+      
+    }); 
+
+    function showCharacters(character) {
+      characterString = characterString + `
+      <div class="box">
+      <img src="${character.image}" class="img">
+<div class="list">
+      <div class="getSingleCharacter" onclick="getSingleCharacter(${character.id})"> ${character.name}</div><br>
+      <div>Última localização conhecida:<br>${character.location.name}</div><br>
+      <div>Genero:<br>${character.gender}</div>
+      </div>
+      </div>
+      </div>
+
+      `
+    }
 }
 
 init();
