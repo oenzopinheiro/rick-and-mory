ca40f6d25170422f4b8aad7d19374aa00e89f499
oenzo_pinheiro <134458981+oenzopinheiro@users.noreply.github.com>
2023-08-22 18:54:49 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,54 +1,98 @@
-const containerElement = document.querySelector(".container");
-const like = document.querySelector(".like");
-let charactersString = "";
+*{
+    margin:  0;
+    padding: 0;
 
-like.addEventListener("click", () => {
-  changeLike();
-});
-
-function changeLike() {
-  like.innerHTML = `
-  <svg fill="#F50000" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M462.3 62.6C407.5 15.9 326 24.3 275.7 76.2L256 96.5l-19.7-20.3C186.1 24.3 104.5 15.9 49.7 62.6c-62.8 53.6-66.1 149.8-9.9 207.9l193.5 199.8c12.5 12.9 32.8 12.9 45.3 0l193.5-199.8c56.3-58.1 53-154.3-9.8-207.9z"/></svg>
-  `;
 }
 
-function init() {
-  fetch("https://rickandmortyapi.com/api/character")
-    .then((response) => {
-      return response.json();
-    })
-    .then((response) => {
-      const { info, results } = response;
-      showItems(results);
-    });
+ section {
+    background: #15151D;
 }
 
-function getSingleCharacter(id) {
-  fetch(`https://rickandmortyapi.com/api/character/${id}`)
-    .then((response) => {
-      return response.json();
-    })
-    .then((response) => {
-      containerElement.innerHTML = `
-        <div>
-            ${response.name}        
-            ${response.gender} 
-            <img src="${response.image}">
-        </div>
-      `;
-    });
+ body {
+    background: #15151D;
+    align-items: center;
+    justify-content: center;
 }
 
-function showItems(characters) {
-  for (let index = 0; index < characters.length; index++) {
-    charactersString += `
-        <button onclick="getSingleCharacter(${characters[index].id})">
-          <span>${characters[index].name}</span>
-        </button>
-      `;
-  }
-
-  containerElement.innerHTML = charactersString;
+ header{
+    display: flex;
 }
 
-init();
+ body a{
+    color: white;
+    font-size: 40px;
+    margin-left: 71px;
+    font-family: Arial, Helvetica, sans-serif;
+}
+
+.all{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+.characters_container{
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 25px;
+} 
+
+.getSingleCharacter{
+    cursor: pointer;
+    font-weight: 700;
+    font-size: 37.899px;
+    color: #ffffff;
+    font-family: Arial, Helvetica, sans-serif;
+    margin-top: 21px;
+
+}
+
+.box{
+    width: 532px;
+    height: 258px;
+    border-radius: 29px;
+    border: 2px solid #535359;
+    display: flex;
+}
+
+.img{
+   width: 230px;
+   height: 230px;
+   padding: 14px 14px 0 14px;
+   border-radius: 29px;
+}
+
+.list{
+    display: flex;
+    flex-direction: column;
+    color: #6F707D;
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 16px;
+    font-weight: 400;
+    text-align: left;
+
+}
+
+#area{
+    color: aliceblue;
+            width: 24px;
+            height: 20px;
+            line-height: 200px;
+            text-align: center;
+}
+
+.rickAndMoryApi{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+    max-width: 1920px;
+} 
+
+.texts{
+    margin-bottom: 61px;
+}
+
+.img1{
+    position: absolute;
+} 
