e8be6ddec24f03c6689962fa40a62d4965867ca7
oenzo_pinheiro <134458981+oenzopinheiro@users.noreply.github.com>
2023-08-22 19:03:03 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,98 +1,56 @@
-*{
-    margin:  0;
-    padding: 0;
+const rickElement = document.querySelector(".rickAndMoryApi");
 
+let characterString = ""
+
+async function getSingleCharacter(id) {
+  fetch(`https://rickandmortyapi.com/api/character/${id}`)
+  .then((response) => {
+    return response.json();
+  })
+  .then((response) => {
+
+
+    rickElement.innerHTML = `
+    <div class="box">
+    <img src="${response.image}" class="img">
+<div class="list">
+    <div class="getSingleCharacter"> ${response.name}</div>
+    </div>
+    </div>
+    `
+  });
 }
 
- section {
-    background: #15151D;
+async function init() {
+  fetch("https://rickandmortyapi.com/api/character")
+    .then((response) => {
+      return response.json();
+    })
+    .then((response) => {
+      const { info, results} = response;
+
+      results.forEach(showCharacters)
+
+      rickElement.innerHTML = `
+      <div class="characters_container">${characterString}
+      </div>`
+      
+    }); 
+
+    function showCharacters(character) {
+      characterString = characterString + `
+      <div class="box">
+      <img src="${character.image}" class="img">
+<div class="list">
+      <div class="getSingleCharacter" onclick="getSingleCharacter(${character.id})"> ${character.name}</div><br>
+      <div>Última localização conhecida:<br>${character.location.name}</div><br>
+      <div>Genero:<br>${character.gender}</div>
+      </div>
+      </div>
+      </div>
+
+      `
+    }
 }
 
- body {
-    background: #15151D;
-    align-items: center;
-    justify-content: center;
-}
-
- header{
-    display: flex;
-}
-
- body a{
-    color: white;
-    font-size: 40px;
-    margin-left: 71px;
-    font-family: Arial, Helvetica, sans-serif;
-}
-
-.all{
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-}
-
-.characters_container{
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    gap: 25px;
-} 
-
-.getSingleCharacter{
-    cursor: pointer;
-    font-weight: 700;
-    font-size: 37.899px;
-    color: #ffffff;
-    font-family: Arial, Helvetica, sans-serif;
-    margin-top: 21px;
-
-}
-
-.box{
-    width: 532px;
-    height: 258px;
-    border-radius: 29px;
-    border: 2px solid #535359;
-    display: flex;
-}
-
-.img{
-   width: 230px;
-   height: 230px;
-   padding: 14px 14px 0 14px;
-   border-radius: 29px;
-}
-
-.list{
-    display: flex;
-    flex-direction: column;
-    color: #6F707D;
-    font-family: Arial, Helvetica, sans-serif;
-    font-size: 16px;
-    font-weight: 400;
-    text-align: left;
-
-}
-
-#area{
-    color: aliceblue;
-            width: 24px;
-            height: 20px;
-            line-height: 200px;
-            text-align: center;
-}
-
-.rickAndMoryApi{
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-wrap: wrap;
-    max-width: 1920px;
-} 
-
-.texts{
-    margin-bottom: 61px;
-}
-
-.img1{
-    position: absolute;
-} 
+init();
